5ebffa2ebeeee1b528ebfd3ab915a314de0d79d7
Monisha <129486568+MonishaKarthikeyan07@users.noreply.github.com>
2024-04-27 12:25:39 +0530

    test.py

--- test.py
+++ test.py
@@ -1,51 +1,65 @@
-# test.py
 import os
 import torch
-import argparse
-from torchvision import transforms
+import numpy as np
 from PIL import Image
 from model import PhysicalNN
+import argparse
+from torchvision import transforms
+import datetime
+import math
+from trpo import TRPO
 
 def main(checkpoint, imgs_path, result_path):
+
     ori_dirs = []
     for image in os.listdir(imgs_path):
         ori_dirs.append(os.path.join(imgs_path, image))
 
+    # Check for GPU
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
+    # Load model
     model = PhysicalNN()
     model = torch.nn.DataParallel(model).to(device)
-
     print("=> loading trained model")
     checkpoint = torch.load(checkpoint, map_location=device)
     model.load_state_dict(checkpoint['state_dict'])
     print("=> loaded model at epoch {}".format(checkpoint['epoch']))
-
+    model = model.module
     model.eval()
 
+    # TRPO instance
+    trpo = TRPO(model)
+
     testtransform = transforms.Compose([
-        transforms.ToTensor(),
-    ])
+                transforms.ToTensor(),
+            ])
     unloader = transforms.ToPILImage()
 
+    starttime = datetime.datetime.now()
     for imgdir in ori_dirs:
-        img_name = os.path.splitext(os.path.basename(imgdir))[0]
+        img_name = (imgdir.split('/')[-1]).split('.')[0]
         img = Image.open(imgdir)
         inp = testtransform(img).unsqueeze(0)
         inp = inp.to(device)
-        out = model(inp)
+        out = trpo.predict(inp)
 
         corrected = unloader(out.cpu().squeeze(0))
-        dir = os.path.join(result_path, 'results_{}'.format(checkpoint['epoch']))
+        dir = '{}/results_{}'.format(result_path, checkpoint['epoch'])
         if not os.path.exists(dir):
             os.makedirs(dir)
-        corrected.save(os.path.join(dir, '{}_corrected.png'.format(img_name)))
+        corrected.save(dir+'/{}corrected.png'.format(img_name))
+    endtime = datetime.datetime.now()
+    print(endtime-starttime)
 
 if __name__ == '__main__':
+
     parser = argparse.ArgumentParser()
     parser.add_argument('--checkpoint', help='checkpoints path', required=True)
-    parser.add_argument('--images', help='test images folder', default='./test_img/')
-    parser.add_argument('--result', help='results folder', default='./results/')
+    parser.add_argument(
+            '--images', help='test images folder', default='./test_img/')
+    parser.add_argument(
+            '--result', help='results folder', default='./results/')
     args = parser.parse_args()
     checkpoint = args.checkpoint
     imgs = args.images
