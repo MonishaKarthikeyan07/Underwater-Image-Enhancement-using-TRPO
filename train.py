087b1378639e30dfa5b9edd7c55564c8ba069198
Monisha <129486568+MonishaKarthikeyan07@users.noreply.github.com>
2024-04-28 20:13:15 +0530

    train.py

--- train.py
+++ train.py
@@ -1,20 +1,15 @@
-import argparse
+import torch
 from trpo import TRPOAgent
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--ori_dir', help='Directory containing original images', required=True)
-    parser.add_argument('--ucc_dir', help='Directory containing transformed images', required=True)
-    parser.add_argument('--batch_size', type=int, default=1, help='Batch size for training')
-    parser.add_argument('--n_workers', type=int, default=0, help='Number of workers for data loading')
-    parser.add_argument('--epochs', type=int, default=100, help='Number of epochs for training')
-    args = parser.parse_args()
-
-    ori_dirs = [args.ori_dir]
-    ucc_dirs = [args.ucc_dir]
+    ori_dirs = ['/content/drive/MyDrive/Dataset/raw']
+    ucc_dirs = ['/content/drive/MyDrive/Dataset/reference-890']
+    batch_size = 32
+    n_workers = 4
+    epochs = 100
 
     trpo_agent = TRPOAgent()
-    trpo_agent.train(ori_dirs, ucc_dirs, args.batch_size, args.n_workers, args.epochs)
+    trpo_agent.train(ori_dirs, ucc_dirs, batch_size, n_workers, epochs)
 
 if __name__ == '__main__':
     main()
