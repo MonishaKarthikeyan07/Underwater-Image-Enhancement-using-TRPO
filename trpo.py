b8e518095103d79064ac4c17818a0c80eab2e1dc
Monisha <129486568+MonishaKarthikeyan07@users.noreply.github.com>
2024-04-28 19:56:16 +0530

    trpo.py

--- trpo.py
+++ trpo.py
@@ -1,45 +1,66 @@
 import torch
-import torch.nn as nn
 from torch.utils.data import DataLoader
 from uwcc import uwcc
 from model import PhysicalNN
+import os
 
 class TRPOAgent:
     def __init__(self):
         self.policy = PhysicalNN()  # Define your policy network
         self.optimizer = torch.optim.Adam(self.policy.parameters(), lr=0.001)
+        self.criterion = torch.nn.MSELoss()
 
     def collect_samples(self, ori_dirs, ucc_dirs, batch_size, n_workers):
         train_set = uwcc(ori_dirs, ucc_dirs, train=True)
         train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=n_workers)
         return train_loader
 
-    def surrogate_loss(self, old_probs, new_probs, advantages):
-        ratio = new_probs / old_probs
-        surr1 = ratio * advantages
-        surr2 = torch.clamp(ratio, 1.0 - 0.2, 1.0 + 0.2) * advantages
-        return -torch.min(surr1, surr2).mean()
-
     def compute_advantages(self, rewards):
         # This is a simple example, you may need to implement a more sophisticated advantage estimation method
         return rewards - rewards.mean()
 
-    def train(self, ori_dirs, ucc_dirs, batch_size, n_workers):
+    def train(self, ori_dirs, ucc_dirs, batch_size, n_workers, epochs):
         dataloader = self.collect_samples(ori_dirs, ucc_dirs, batch_size, n_workers)
 
-        for batch in dataloader:
-            states, actions, rewards = batch
-            old_probs = self.policy(states)
+        best_loss = 9999.0
+        for epoch in range(epochs):
+            losses = self.train_epoch(dataloader)
+            tloss = losses.avg
 
-            # Compute advantages
-            advantages = self.compute_advantages(rewards)
+            print('Epoch:[{}/{}] Loss{}'.format(epoch, epochs, tloss))
+            is_best = tloss < best_loss
+            best_loss = min(tloss, best_loss)
 
-            # Policy gradient ascent
-            for _ in range(10):  # TRPO typically uses a line search or conjugate gradient
-                new_probs = self.policy(states)
-                loss = self.surrogate_loss(old_probs, new_probs, advantages)
-                self.optimizer.zero_grad()
-                loss.backward()
-                self.optimizer.step()
+            # Save checkpoint
+            if is_best:
+                self.save_checkpoint(epoch)
 
-        return loss.item()
+        print('Best Loss: ', best_loss)
+
+    def train_epoch(self, dataloader):
+        losses = AverageMeter()
+        self.policy.train()
+
+        for i, sample in enumerate(dataloader):
+            ori, ucc = sample
+
+            corrected = self.policy(ori)
+            loss = self.criterion(corrected, ucc)
+            losses.update(loss.item())
+
+            self.optimizer.zero_grad()
+            loss.backward()
+            self.optimizer.step()
+
+        return losses
+
+    def save_checkpoint(self, epoch):
+        filename = './checkpoints/model_tmp.pth.tar'
+        if not os.path.exists('./checkpoints'):
+            os.makedirs('./checkpoints')
+
+        torch.save({
+            'epoch': epoch + 1,
+            'state_dict': self.policy.state_dict(),
+            'optimizer': self.optimizer.state_dict(),
+        }, filename)
